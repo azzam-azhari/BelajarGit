b41049c4f7791dccca7b2b92058b6cff947ca0a4
Azzam Azhari <azzamazhari.dev@gmail.com>
2025-01-15 20:32:19 +0700

    menambahkan fitur perkalian

--- fungsi.js
+++ fungsi.js
@@ -2,3 +2,7 @@ const add = (a, b) => a + b;
 console.log(add(1, 2));
 
 const subtrack = (a, b) => a - b;
+console.log(subtrack(5, 2));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 2));
